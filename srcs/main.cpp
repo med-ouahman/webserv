f7001da731777064659d35e3b8f4b02998b48f03
med-ouahman <mohamedouahmane550@gmail.com>
2026-01-30 06:55:31 +0100

    conf && tests

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,35 +1,18 @@
-#include <iostream>
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include <cstring>
-
-static int usage( void ) {
-	int ERR_USAGE = 1; /* WILL BE TRANSFORMED TO A MACRO */
-	std::cerr << "Usage: ./webserv [ <config_file> ]\n";
-	return ERR_USAGE;
-}
-
-
-int main( int argc, char** argv ) {
-
-	if (argc > 2) {
-		return usage();
-	}
-	int epollFd = epoll_create1(0);
-	if (epollFd == -1) {
-		std::cerr << "Error creating epoll file descriptor\n";
-		return 1;
-	}
-	struct epoll_event s;
-	memset(&s, 0, sizeof(s));
-	return 0;
-}
-
 
 /*
-
-man, this shit rocks 
-I will finish this and then go to bed
-yep
-
+    Load configuration from a file and initialize application settings.
 */
+#include <iostream>
+
+int main( int argc, char* argv[] ) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+        return 1;
+    }
+    const char* configFile = argv[1];
+    if (!configFile) {
+        configFile = "default.conf";
+        return 1;
+    }
+    return 0;
+}
