13a11a573531a8c53e293346f7775854c6ddcc28
med-ouahman <mohamedouahmane550@gmail.com>
2026-05-10 11:28:50 +0100

    I need a break

--- srcs/http/application/dispatcher/HTTPDispatcher.hpp
+++ srcs/http/application/dispatcher/HTTPDispatcher.hpp
@@ -17,31 +17,29 @@ namespace http {
 
     struct HTTPRequest;
 
-    class HTTPDispatcher {
+    struct ResolutionResult {
+        HTTPResponseType::Type type;
+        HTTPStatusCode status_code;
+        std::string path;
+        std::string reason;
+        
+        ResolutionResult(): status_code(NONE) {}
+        ResolutionResult( HTTPStatusCode code, std::string r )
+            : status_code(code), reason(r) {}
+    };
 
+
+
+
+
+
+
+    class HTTPDispatcher {
         private:
             const config::Config& config;
             bool allow_keep_alive;
 
-            struct ResolutionResult {
-                HTTPResponseType::Type type;
-                HTTPStatusCode status_code;
-                std::string path;
-                std::string reason;
-                
-                ResolutionResult(): status_code(NONE) {}
-                ResolutionResult( HTTPStatusCode code, std::string r )
-                    : status_code(code), reason(r) {}
-            };
-
-        public:
-            struct HandlerResult {
-                HTTPResponseType::Type response_type;
-                CGIContext cgi_ctx;
-            };
-
         private:
-            ResolutionResult resolve( const HTTPRequest& req, const config::ServerConfig& server );
             static const config::LocationConfig* find_location( const std::string& url,
                 const std::vector<config::LocationConfig>& locations );
             std::string extract_path( const std::string& url );
@@ -49,14 +47,12 @@ namespace http {
             CGIContext get_cgi_context( const HTTPRequest& req, const ResolutionResult& result );
 
         public:
-            std::string generate_directory_list( const char* dir );
-            std::string generate_anchor( const char* name );
-            void build_error_response( HTTPStatusCode code, std::string reason );
-
-        public:
+            std::string generate_directory_list( const char* dir ); // TO BE MOVED
+            std::string generate_anchor( const char* name ); // TO BE MOVED
+            void build_error_response( HTTPStatusCode code, std::string reason ); // TO BE MOVED;
+            ResolutionResult resolve( const HTTPRequest& req, const config::ServerConfig& server );
             HTTPDispatcher( const config::Config& conf );
             ~HTTPDispatcher();
-            HandlerResult handle_request( const HTTPRequest& req );
             bool allow_presistance() { return allow_keep_alive; };
             
     };
