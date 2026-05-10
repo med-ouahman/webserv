99b4e95c0f9f26e12f02c5c9933d373603c3251d
med-ouahman <mohamedouahmane550@gmail.com>
2026-05-10 15:05:40 +0100

    epool'

--- epoll/epoll.c
+++ epoll/epoll.c
@@ -5,23 +5,67 @@
 #include <assert.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+
 #define MAX_EVENTS 10
 #define MAX_INP_PIPES 4
 
+void read_fr( int f ){
+    char b[101];
+    printf("> ");
+    b[read(f, b, 100)] = 0;
+    printf("%s\n", b);
+}
+
+void write_fr( int fd ) {
+    char b[100];
+    read(0, b, 100);
+    write(fd, b, 100);
+}
+
 int main( int argc, char* argv[] ) {
     
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    assert(socket_fd != -1);
-    int epfd = epoll_create1(0);
+    int epfd = epoll_create1(EPOLL_CLOEXEC);
 
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = socket_fd;
-    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, socket_fd, &ev);
-    assert(ret != -1);
-    int r = epoll_wait(epfd, &ev, MAX_EVENTS, -1);
-    assert(r != -1);
+    struct epoll_event ev1, ev2;
+    int fds[4];
+    pipe(fds);
+
+    int a = fork();
+    if (fork() == 0) {
+        dup2(fds[1] , 1);
+        close(fds[0]);
+        close(fds[1]);
+
+        execve("./p", argv, __environ);
+        _exit(1);
+    }
+
+    ev1.events = EPOLLOUT;
+    ev1.data.fd = fds[0];
+
+    ev2.events = EPOLLIN;
+    ev2.data.fd = fds[1];
+    epoll_ctl(epfd, EPOLL_CTL_ADD, fds[0], &ev1);
+    epoll_ctl(epfd, EPOLL_CTL_ADD, fds[1], &ev2);
+
+    struct epoll_event events[MAX_EVENTS];
+    while (true) {
+        int n = epoll_wait(epfd, events, MAX_EVENTS, -1);
+
+        printf("n: %d\n", n);
+        for ( int i = 0; i < n; i++ ) {
+            if (events[i].events&EPOLLIN) {
+                read_fr(events[i].data.fd);
+            } else {
+                write_fr(events[i].data.fd);
+            }
+        }
+    }
+
+    waitpid(a, 0, 0);
+
     close(epfd);
-    close(socket_fd);
     return 0;
 }
